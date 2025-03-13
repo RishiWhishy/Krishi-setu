7da7ac292d9c60538a42474306b143e1960deb9a
RishiWhishy <rishiragul0@gmail.com>
2025-03-13 14:15:15 +0530

    null

--- contractFarming/server.js
+++ contractFarming/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'dps123',
+  password: 'Rishi@2005',
   database: 'sgt'
 });
 
@@ -95,5 +95,3 @@ app.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 });
-
-
